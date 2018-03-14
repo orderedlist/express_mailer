a02654eeed7ee08bd91934f69430847bf6e09dc4
Steve Smith <steve@orderedlist.com>
2018-03-14 13:38:52 -0400

    Update dummy footer

--- test/dummy/config/initializers/express_mailer.rb
+++ test/dummy/config/initializers/express_mailer.rb
@@ -2,5 +2,5 @@ ExpressMailer.configure do |config|
   config.default_to = "me@test.com"
   config.app_name = "Test Application"
   config.app_url = "http://test.localhost"
-  config.default_footer = "You received this email because you are an administrator. <a href=\"http://example.com/admin\">View the admin</a>."
+  config.default_footer = "You received this email because it was sent to you by our application. If you want, you can <a href=\"http://example.com/admin\">manage your notifications</a>."
 end
